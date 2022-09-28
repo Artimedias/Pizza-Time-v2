7479e9bdb6ec1839207d7fd77152621c4959b98d
Artimedias <brettroper22@gmail.com>
2022-09-27 17:47:50 -0700

    Fixed issue where I couldnt define things using the doc because the webpage hadn't loaded yet

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,32 @@
 let allPizzas = [];
 
 
-function pizza(size, sauce, flavor, toppings, dressing) {
+function pizza(size, sauce, toppings, dressing) {
     this.size = size;
     this.sauce = sauce;
-    this.flavor = flavor;
     this.toppings = toppings;
     this.dressing = dressing;
   }
+
+
+  //UI logic
+
+  window.addEventListener("load", runapp);
+function runapp() 
+{
+  let size;
+  let sauce;
+  let toppings = [];
+  let dressing;
+
+  const order = document.getElementById("Order");
+  console.log( document.getElementById("Order"));
+
+
+  order.addEventListener("click", sizePicker());
+
+  function sizePicker()
+  {
+    console.log("Working!")
+  }
+}
