b812bba0564a07185988e76209acb1df7bdf9128
Artimedias <brettroper22@gmail.com>
2022-09-27 22:49:51 -0700

    Got all the toppings options added, now making a function that will turn it into an object

--- js/scripts.js
+++ js/scripts.js
@@ -34,7 +34,7 @@ function runapp()
   const herbs = document.getElementById("Herbsbtn");
   const dressing = document.getElementById("Dressingbtn");
 
-  //These are the divs that handle each part of the order
+  //These are the divs that handle each part of the order. C stands for class, even though I ended up using ids instead, I wanted to use I for input.
   const sizeC = document.getElementById("sizeC");
   const sauceC = document.getElementById("sauceC");
   const cheeseC = document.getElementById("cheeseC");
@@ -45,6 +45,7 @@ function runapp()
   const veggietwoC = document.getElementById("veggietwoC");
   const herbsC = document.getElementById("herbsC");
   const dressingC = document.getElementById("dressingC");
+  const finalC = document.getElementById("finalC");
 
 
 
@@ -53,6 +54,14 @@ function runapp()
   order.addEventListener("click", sizePicker);
   size.addEventListener("click", sizeStuff)
   sauce.addEventListener("click", sauceStuff)
+  cheese.addEventListener("click", cheeseStuff)
+  cheesetwo.addEventListener("click", cheeseTwoStuff)
+  meat.addEventListener("click", meatStuff)
+  meattwo.addEventListener("click", meatTwoStuff)
+  veggie.addEventListener("click", veggieStuff)
+  veggietwo.addEventListener("click", veggieTwoStuff)
+  herbs.addEventListener("click", herbsStuff)
+  dressing.addEventListener("click", dressingStuff)
 
 
   //These are the functions that handle calling the toggler for various things. Im not quite sure why I cant just call it with the event listener directly though, if I could get some feedback on that it would be greatly appreciated
@@ -72,23 +81,103 @@ function runapp()
 
   function sauceStuff()
   {
-    
+    sauceL = document.getElementById("sauceI").options[document.getElementById("sauceI").selectedIndex].value;
+    toggler(sauceC)
+    toggler(cheeseC)
   }
+
+  function cheeseStuff()
+{
+    toppingsL[0] = document.getElementById("cheeseI").options[document.getElementById("cheeseI").selectedIndex].value;
+    if(toppingsL[0] === "No Cheese")
+    {
+        toppingsL[1] = "No Cheese"
+        toggler(meatC)
+    }
+    else
+    {
+        toggler(cheesetwoC)
+    }
+    toggler(cheeseC)
+
+}
+
+function cheeseTwoStuff()
+{
+    toppingsL[1] = document.getElementById("cheesetwoI").options[document.getElementById("cheesetwoI").selectedIndex].value;
+  toggler(cheesetwoC)
+  toggler(meatC)
+}
+
+function meatStuff()
+{
+    toppingsL[2] = document.getElementById("meatI").options[document.getElementById("meatI").selectedIndex].value;
+    if(toppingsL[2] === "No Meats")
+    {
+        toppingsL[3] = "No Meats"
+        toggler(veggieC)
+    }
+    else
+    {
+        toggler(meattwoC)
+    }
+    toggler(meatC)
+}
+
+function meatTwoStuff()
+{
+    toppingsL[3] = document.getElementById("meattwoI").options[document.getElementById("meattwoI").selectedIndex].value;
+  toggler(meattwoC)
+  toggler(veggieC)
+}
+
+function veggieStuff()
+{
+    toppingsL[4] = document.getElementById("veggieI").options[document.getElementById("veggieI").selectedIndex].value;
+    if(toppingsL[4] === "No Veggies")
+    {
+        toppingsL[5] = "No Veggies"
+        toggler(herbsC)
+    }
+    else
+    {
+        toggler(veggietwoC)
+    }
+    toggler(veggieC)
+}
+
+function veggieTwoStuff()
+{
+    toppingsL[5] = document.getElementById("veggietwoI").options[document.getElementById("veggietwoI").selectedIndex].value;
+  toggler(veggietwoC)
+  toggler(herbsC)
+}
+
+function herbsStuff()
+{
+    toppingsL[6] = document.getElementById("herbsI").options[document.getElementById("herbsI").selectedIndex].value;
+  toggler(herbsC)
+  toggler(dressingC)
+}
+
+function dressingStuff()
+{
+    dressingL = document.getElementById("dressingI").options[document.getElementById("dressingI").selectedIndex].value;
+  toggler(dressingC)
+  toggler(finalC)
+    console.log(toppingsL)
+}
 
  
 
   function toggler(element)
   {
-    console.log(element);
-    console.log(element.style.visibility)
     if(element.style.visibility  === "visible")
     {
-        console.log("reached!")
         element.style.visibility  = "collapse";
         
     }else{
         element.style.visibility  = "visible";
-        console.log("reached! - bad")
     }
   }
 }
