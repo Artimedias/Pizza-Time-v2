e37b1437707fb01815d26075d66f723ee455cc44
Artimedias <brettroper22@gmail.com>
2022-09-27 17:23:20 -0700

    Set up pizza object

---
+++ js/scripts.js
@@ -0,0 +1,10 @@
+let allPizzas = [];
+
+
+function pizza(size, sauce, flavor, toppings, dressing) {
+    this.size = size;
+    this.sauce = sauce;
+    this.flavor = flavor;
+    this.toppings = toppings;
+    this.dressing = dressing;
+  }
