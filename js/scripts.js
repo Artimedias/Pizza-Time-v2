1b188b8ec5d3df6c9fd1cc19b44eca77f3181c10
Artimedias <brettroper22@gmail.com>
2022-09-27 19:55:16 -0700

    setting up stuff so that the responses from the dropdown menus now actually give back a value

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,7 @@
+//An array of all pizzas. This will be used later to print out the orders.
 let allPizzas = [];
 
-
+//The pizza object
 function pizza(size, sauce, toppings, dressing) {
     this.size = size;
     this.sauce = sauce;
@@ -14,23 +15,26 @@ function pizza(size, sauce, toppings, dressing) {
   window.addEventListener("load", runapp);
 function runapp() 
 {
+    //These are the values that will be used to make the pizza object later
   let sizeL;
   let sauceL;
   let toppingsL = [];
   let dressingL;
 
+  //These are the next buttons
   const order = document.getElementById("Orderbtn");
-  const size = document.getElementById("Size");
-  const sauce = document.getElementById("Sauce");
-  const cheese = document.getElementById("Cheese");
-  const cheesetwo = document.getElementById("CheeseTwo");
-  const meat = document.getElementById("Meat");
-  const meattwo = document.getElementById("MeatTwo");
-  const veggie = document.getElementById("Veggie");
-  const veggietwo = document.getElementById("VeggieTwo");
-  const herbs = document.getElementById("Herbs");
-  const dressing = document.getElementById("Dressing");
+  const size = document.getElementById("Sizebtn");
+  const sauce = document.getElementById("Saucebtn");
+  const cheese = document.getElementById("Cheesebtn");
+  const cheesetwo = document.getElementById("CheeseTwobtn");
+  const meat = document.getElementById("Meatbtn");
+  const meattwo = document.getElementById("MeatTwobtn");
+  const veggie = document.getElementById("Veggiebtn");
+  const veggietwo = document.getElementById("VeggieTwobtn");
+  const herbs = document.getElementById("Herbsbtn");
+  const dressing = document.getElementById("Dressingbtn");
 
+  //These are the divs that handle each part of the order
   const sizeC = document.getElementById("sizeC");
   const sauceC = document.getElementById("sauceC");
   const cheeseC = document.getElementById("cheeseC");
@@ -44,24 +48,46 @@ function runapp()
 
 
 
-  order.addEventListener("click", sizePicker);
 
+
+  order.addEventListener("click", sizePicker);
+  size.addEventListener("click", sizeStuff)
+  sauce.addEventListener("click", sauceStuff)
+
+
+  //These are the functions that handle calling the toggler for various things. Im not quite sure why I cant just call it with the event listener directly though, if I could get some feedback on that it would be greatly appreciated
   function sizePicker()
   {
     toggler(sizeC)
-    //sizeC.style.visibility = "visible";
   }
+
+    //These are the functions that give actual values to the Ls
+  function sizeStuff()
+  {
+    //this giant line here just tells it how to get the thing it needs
+      sizeL = document.getElementById("sizeI").options[document.getElementById("sizeI").selectedIndex].value;
+      toggler(sizeC)
+      toggler(sauceC)
+  }
+
+  function sauceStuff()
+  {
+    
+  }
+
+ 
 
   function toggler(element)
   {
     console.log(element);
     console.log(element.style.visibility)
-    if(element.style.visibility  === "collapse")
+    if(element.style.visibility  === "visible")
     {
         console.log("reached!")
-        element.style.visibility  = "visible";
-    }else{
         element.style.visibility  = "collapse";
+        
+    }else{
+        element.style.visibility  = "visible";
         console.log("reached! - bad")
     }
   }
